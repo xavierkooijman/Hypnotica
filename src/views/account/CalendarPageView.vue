f225962a2dbe5ffded1662b4a859b7ab70ac0a24
Xavier Kooijman <xavierkooijman@gmail.com>
2025-01-24 03:11:08 +0000

    calendar html,css and apply height based on event duration

--- src/views/account/CalendarPageView.vue
+++ src/views/account/CalendarPageView.vue
@@ -1,19 +1,129 @@
-<template>
-    <div>
-        <h1>Calendar</h1>
-        <Sidebar/>
-    </div>
-</template>
-
 <script>
+import { useEventStore } from "@/stores/events";
+import { useUsersStore } from "@/stores/user";
 import Sidebar from '@/components/Sidebar.vue';
     export default {
         components: {
             Sidebar,
         },
+        data() {
+            return {
+                userStore: useUsersStore(),
+                eventStore: useEventStore(),
+								hours: ["20:00", "21:00", "22:00", "23:00", "00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00"]
+            }
+        },
     }
 </script>
 
+<template>
+  <div class="sidebar-calendar-container">
+    <Sidebar/>
+		<div class="days-hours-container">
+			<div class="hours-container">
+				<div v-for="hour in hours" class="hour">{{ hour }}</div>
+			</div>
+			<div class="days-blocks-container">
+				<div class="days-container">
+					<p class="day first-day">Friday, 24th January</p>
+      		<p class="day">Saturday, 25th January</p>
+      		<p class="day">Sunday, 26th January</p>
+				</div>
+				<div class="blocks-container">
+					<div class="calendar-block" v-for="n in 30"></div>
+					<div class="event-card" v-for="event in eventStore.events" :style="{height: event.timeDifference * 10 + '%'}">
+						<p>{{ event.name }}</p>
+						<p>{{ event.timeStart }} - {{ event.timeEnd }}</p>
+						<p>{{ event.venueId }}</p>
+					</div>
+				</div>
+			</div>
+		</div>
+  </div>
+</template>
+
 <style scoped>
+
+.sidebar-calendar-container{
+    display: flex;
+    flex-direction: row;
+    gap: 64px;
+		padding-right: 48px;
+		margin-bottom: 128px;
+}
+
+.event-card{
+	position: absolute;
+	display: flex;
+	flex-direction: column;
+	gap: 8px;
+	background-color: var(--mainBlack);
+	border: 1px solid var(--gray100);
+	padding: 12px;
+	border-radius: 16px;
+	width: 32%;
+	top: 30%;
+}
+
+.days-blocks-container{
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	height: 100%;
+}
+
+.calendar-block{
+		border: 1px solid var(--gray500);
+		height: 64px;
+		height: 100%;
+}
+
+.hour{
+	font-family: Aspekta300;
+	font-size: 1rem;
+	color: var(--gray100);
+}
+
+.hours-container{
+	display: flex;
+	flex-direction: column;
+	gap: 64px;
+	margin-top: 52px;
+}
+
+.days-hours-container{
+	display: flex;
+	flex-direction: row;
+	width: 100%;
+	gap: 24px;
+}
+
+.days-container{
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    width: 100%;
+}
+
+.blocks-container{
+	position: relative;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+	grid-template-rows: repeat(10, 1fr);
+  width: 100%;
+	height: 100%;
+}
+
+.day{
+    font-family: Aspekta300;
+    font-size: 1rem;
+		border-bottom: 1px solid var(--gray500);
+		border-right: 1px solid var(--gray500);
+		padding: 16px;
+		text-align: center;
+}
+
+.first-day{
+		border-left: 1px solid var(--gray500);
+}
 
 </style>
